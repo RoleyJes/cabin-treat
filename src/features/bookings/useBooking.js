044c79baf33f516afa5ab531967c2562fd7330aa
Jessica <jessicaimoesemi@gmail.com>
2026-01-03 22:59:50 +0100

    single booking page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+  const {
+    isPending,
+    data: bookings,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    queryFn: () => getBooking(),
+  });
+
+  return { isPending, bookings, error };
+}
