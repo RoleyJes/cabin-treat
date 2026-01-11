2d03c49f3b2acd83dafd44a124edfe79dec8a0bf
Jessica <jessicaimoesemi@gmail.com>
2026-01-11 15:55:30 +0100

    signup form setup

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -12,10 +12,10 @@ export function useLogin() {
 
     // on the onSuccess handler, we can get the data that was received from the fn as an input
     onSuccess: (user) => {
-      // console.log(user);
+      console.log(user);
 
       // This allows us to manually set some data into the react query cache. Here, we're setting the user immediately after login so that the user data will not be fetched again using useUser. RQ will immediately see it in the cache and load the user data from there, and only use useUser when it isn't the case of an immediate log in. This is possiblr because of the queryKey which is the same "user"
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
 
