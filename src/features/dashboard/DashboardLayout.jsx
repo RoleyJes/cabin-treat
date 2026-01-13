f5d42628f488e0f5390a9b4274aab0176b2e6bbd
Jessica <jessicaimoesemi@gmail.com>
2026-01-13 21:58:53 +0100

    stats

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { useRecentBookings } from "./useRecentBookings";
 import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -12,15 +14,25 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isPending: isPending1 } = useRecentBookings();
-  const { stays, confirmedStays, isPending: isPending2 } = useRecentStays();
+  const {
+    stays,
+    confirmedStays,
+    isPending: isPending2,
+    numDays,
+  } = useRecentStays();
 
-  if (isPending1 || isPending2) return <Spinner />;
+  const { cabins, isPending: isPending3 } = useCabins();
 
-  console.log(stays);
+  if (isPending1 || isPending2 || isPending3) return <Spinner />;
 
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Toay's activity</div>
       <div>Chart stay duration</div>
       <div>Chart sales</div>
